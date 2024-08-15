f82b8733759ed0349f8ca6b71183f119236cc3d2
Evgeniy Arkov <arhasik1905@mail.ru>
2024-08-15 19:23:29 +0500

    task1

---
+++ variables/task1/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+//Создайте три переменные в Kotlin:
+//    •	Целочисленную переменную a и присвойте ей значение 10.
+//    •	Строковую переменную name с именем вашего любимого героя.
+//    •	Логическую переменную isStudent и установите ее в true.
+fun main() {
+    val a: Int = 10
+    val name: String = "Tral"
+    val isStudent: Boolean = true
+    println("$a, $name, $isStudent")
+}
