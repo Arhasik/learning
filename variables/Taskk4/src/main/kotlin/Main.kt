cda8322371fb5e1fd4ef9e073d5445d30252c494
Evgeniy Arkov <arhasik1905@mail.ru>
2024-08-18 18:26:44 +0500

    Task 4

---
+++ variables/Taskk4/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+//    Объявите переменные следующих типов:
+//    •	Переменную age типа Int и присвойте ей значение 25.
+//    •	Переменную height типа Float и присвойте ей значение 180.5.
+//    •	Переменную isEmployed типа Boolean и установите ее в false.
+fun main() {
+    val age: Int = 25
+    val height: Float = 180.5f
+    val isEmployed: Boolean = false
+}
