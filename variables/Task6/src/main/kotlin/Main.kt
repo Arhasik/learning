bc65f4020fa5744347f06ce7a337f1fe482a1666
Evgeniy Arkov <arhasik1905@mail.ru>
2024-08-18 19:34:37 +0500

    Task 6

---
+++ variables/Task6/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+//Объявите две переменные типа Int, a и b, и присвойте им любые значения.
+//Найдите их сумму, разность, произведение и частное, и выведите результаты на экран.
+fun main() {
+    val a: Int = 2
+    val b: Int = 5
+    val difference: Int = b - a
+    val productOfNumbers: Int = a * b
+    val  dividing: Int = (b / a)
+    println("Difference = $difference, product of numbers = $productOfNumbers, dividing = $dividing")
+}
