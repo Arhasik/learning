d748d528befc39e57a3b865d50e032f3ede2d96b
Evgeniy Arkov <arhasik1905@mail.ru>
2024-08-19 12:55:24 +0500

    Task 7

---
+++ variables/Task7/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+
+//Создайте две переменные типа String, firstName и lastName. Присвойте им ваше имя и фамилию соответственно.
+//Создайте третью переменную fullName, которая будет содержать полное имя в формате "Имя Фамилия". Выведите fullName на экран.
+fun main() {
+    val firstName: String = "Evgeniy"
+    val lastName :String = "Arkov"
+    val fullName: String = firstName + " " + lastName
+    println(fullName)
+}
